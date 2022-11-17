57f922f4efa3691f4cdd69344458a702344e7e81
Михайло Грошевий <43928021+Misha999777@users.noreply.github.com>
2022-11-17 20:48:51 +0100

    fixed access token decoding

--- src/utils/TokenUtils.js
+++ src/utils/TokenUtils.js
@@ -2,7 +2,33 @@ export function extractRoles(session) {
     let accessToken = JSON.parse(session).access_token;
     let tokenPayload = accessToken.split(".")[1];
 
-    let decodedPayload = atob(tokenPayload);
+    let decodedPayload = atob(toBase64(tokenPayload));
 
     return JSON.parse(decodedPayload).realm_access?.roles;
+}
+
+function toBase64(base64url) {
+    base64url = base64url.toString();
+    
+    return padString(base64url)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+}
+
+function padString(input) {
+    let segmentLength = 4;
+    let stringLength = input.length;
+    let diff = stringLength % segmentLength;
+
+    if (!diff) {
+        return input;
+    }
+
+    let padLength = segmentLength - diff;
+
+    while (padLength--) {
+        input += "=";
+    }
+
+    return input;
 }
